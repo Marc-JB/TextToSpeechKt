d1309dccd1d82f29d16ac7dd7bdf37f38bce4583
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-08-07 15:15:20 +0200

    Bugfixes

--- src/androidMain/kotlin/nl/marc/tts/TextToSpeechAndroid.kt
+++ src/androidMain/kotlin/nl/marc/tts/TextToSpeechAndroid.kt
@@ -10,7 +10,7 @@ import java.util.*
 import android.speech.tts.TextToSpeech as AndroidTTS
 
 @TargetApi(VERSION_CODES.DONUT)
-internal class TextToSpeechAndroid(private val tts: AndroidTTS) : TextToSpeechInstance {
+internal class TextToSpeechAndroid(private var tts: AndroidTTS?) : TextToSpeechInstance {
     private val internalVolume: Float
         get() = if(!isMuted) volume / 100f else 0f
 
@@ -35,17 +35,17 @@ internal class TextToSpeechAndroid(private val tts: AndroidTTS) : TextToSpeechIn
     override var pitch: Float = 1f
         set(value) {
             field = value
-            tts.setPitch(value)
+            tts?.setPitch(value)
         }
 
     override var rate: Float = 1f
         set(value) {
             field = value
-            tts.setSpeechRate(value)
+            tts?.setSpeechRate(value)
         }
 
     private val voiceLocale: Locale
-        get() = if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) tts.voice.locale else tts.language
+        get() = (if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) tts?.voice?.locale else tts?.language) ?: Locale.getDefault()
 
     /**
      * Returns a BCP 47 language tag of the selected voice on supported platforms.
@@ -73,24 +73,25 @@ internal class TextToSpeechAndroid(private val tts: AndroidTTS) : TextToSpeechIn
         if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
             val params = Bundle()
             params.putFloat(KEY_PARAM_VOLUME, internalVolume)
-            tts.speak(text, queueMode, params, null)
+            tts?.speak(text, queueMode, params, null)
         } else {
             val params = hashMapOf(
                 KEY_PARAM_VOLUME to internalVolume.toString()
             )
-            tts.speak(text, queueMode, params)
+            tts?.speak(text, queueMode, params)
         }
     }
 
     /** Clears the internal queue, but doesn't close used resources. */
     override fun stop() {
-        tts.stop()
+        tts?.stop()
     }
 
     /** Clears the internal queue and closes used resources (if possible) */
     override fun close() {
-        tts.stop()
-        tts.shutdown()
+        tts?.stop()
+        tts?.shutdown()
+        tts = null
     }
 
     companion object {
