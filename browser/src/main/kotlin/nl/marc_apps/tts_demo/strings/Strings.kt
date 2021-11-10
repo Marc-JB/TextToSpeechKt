65c45f905be45eba91e9c3bf8ca77dd2fc835cd2
Marc <16156117+Marc-JB@users.noreply.github.com>
2021-11-10 20:31:44 +0100

    Add support for the Dutch language in the Android and browser demo

---
+++ browser/src/main/kotlin/nl/marc_apps/tts_demo/strings/Strings.kt
@@ -0,0 +1,15 @@
+package nl.marc_apps.tts_demo.strings
+
+interface Strings {
+    val pageTitle: String
+
+    val ttsTextLabel: String
+
+    val ttsTextDefaultValue: String
+
+    val ttsVolumeLabel: String
+
+    fun ttsVolumeLevel(level: Int): String
+
+    val actionSay: String
+}
