27bb30314479ef28fb30e2d83378561152d85c8b
Marc <16156117+Marc-JB@users.noreply.github.com>
2021-11-10 13:51:55 +0100

    Add volume control to browser demo

--- browser/src/main/kotlin/nl/marc_apps/tts_demo/TtsDemoApplication.kt
+++ browser/src/main/kotlin/nl/marc_apps/tts_demo/TtsDemoApplication.kt
@@ -3,14 +3,15 @@ package nl.marc_apps.tts_demo
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.launch
-import kotlinx.html.id
+import kotlinx.html.*
 import kotlinx.html.js.*
+import kotlinx.html.js.br
 import kotlinx.html.js.onClickFunction
-import kotlinx.html.textInput
 import nl.marc_apps.tts.TextToSpeech
 import nl.marc_apps.tts.TextToSpeechInstance
 import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.HTMLSpanElement
 import kotlin.properties.Delegates
 
 class TtsDemoApplication : SuspendingApplication() {
@@ -18,6 +19,14 @@ class TtsDemoApplication : SuspendingApplication() {
 
     private val inputTtsText by lazy {
         document.getElementById(INPUT_TTS_TEXT_ID) as HTMLInputElement
+    }
+
+    private val inputTtsVolume by lazy {
+        document.getElementById(INPUT_TTS_VOLUME_ID) as HTMLInputElement
+    }
+
+    private val labelTtsVolume by lazy {
+        document.getElementById(LABEL_TTS_VOLUME_ID) as HTMLSpanElement
     }
 
     private val actionSay by lazy {
@@ -49,15 +58,42 @@ class TtsDemoApplication : SuspendingApplication() {
                 }
                 br()
                 br()
+                label {
+                    +"Volume: "
+                    htmlFor = INPUT_TTS_VOLUME_ID
+                }
+                rangeInput {
+                    value = 100.toString()
+                    min = 0.toString()
+                    max = 100.toString()
+                    id = INPUT_TTS_VOLUME_ID
+                    name = INPUT_TTS_VOLUME_ID
+
+                    onInputFunction = {
+                        labelTtsVolume.textContent = " (${inputTtsVolume.value.toIntOrNull()}%)"
+                    }
+                }
+                span {
+                    +" (100%)"
+                    id = LABEL_TTS_VOLUME_ID
+                }
+                br()
+                br()
                 button {
                     +"Say"
                     id = ACTION_SAY_ID
                     disabled = true
                     onClickFunction = {
-                        if(inputTtsText.value.isNotBlank()) {
+                        inputTtsVolume.value.toIntOrNull()?.let {
+                            tts?.volume = it
+                        }
+
+                        val ttsText = inputTtsText.value
+
+                        if(ttsText.isNotBlank()) {
                             isTtsLoading = true
                             launch {
-                                tts?.say(inputTtsText.value)
+                                tts?.say(ttsText)
                                 isTtsLoading = false
                             }
                         }
@@ -76,5 +112,9 @@ class TtsDemoApplication : SuspendingApplication() {
         private const val ACTION_SAY_ID = "action_say"
 
         private const val INPUT_TTS_TEXT_ID = "input_tts_text"
+
+        private const val INPUT_TTS_VOLUME_ID = "input_tts_volume"
+
+        private const val LABEL_TTS_VOLUME_ID = "label_tts_volume"
     }
 }
