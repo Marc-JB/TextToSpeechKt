e174ffce4b9786d2e3f5497fb26021ef1a76d56f
Marc <16156117+Marc-JB@users.noreply.github.com>
2021-11-14 13:36:26 +0100

    Set version to 1.0.0 stable

--- tts/build.gradle.kts
+++ tts/build.gradle.kts
@@ -16,7 +16,7 @@ object ProjectInfo {
 
     const val NAME = "TextToSpeechKt"
 
-    const val VERSION = "0.10.0"
+    const val VERSION = "1.0.0"
 
     object Developer {
         const val ORG_NAME = "Marc Apps & Software"
