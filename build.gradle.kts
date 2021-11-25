dc1539a85613ffbc050e0a885325949c08e4870d
Marc <16156117+Marc-JB@users.noreply.github.com>
2021-11-25 12:03:10 +0100

    Upgrade Dokka to version 1.6 (from 1.5.31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,6 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
         classpath("com.android.tools.build:gradle:7.0.3")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.5.31")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.6.0")
     }
 }
