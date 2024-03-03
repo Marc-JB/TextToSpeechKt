d76783d56daec583bed02dde64974d48833696ab
Marc <16156117+Marc-JB@users.noreply.github.com>
2024-03-03 16:50:44 +0100

    Use edge-to-edge for Android

--- demo/compose-multiplatform/src/androidMain/kotlin/nl/marc_apps/tts_demo/MainActivity.kt
+++ demo/compose-multiplatform/src/androidMain/kotlin/nl/marc_apps/tts_demo/MainActivity.kt
@@ -3,6 +3,7 @@ package nl.marc_apps.tts_demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -11,6 +12,8 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             TtsDemoApp(
