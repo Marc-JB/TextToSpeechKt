7cc17726abdedec14bfbeb7810cc03f363b7b4ac
Dharmendra Pratap Singh <dharmendrascpm@gmail.com>
2024-03-14 21:45:44 +0000

    Improve TextToSpeechAndroid cancellation behavior with suspendCancellableCoroutine (#1)
    * Improve TextToSpeechAndroid cancellation behavior with suspendCancellableCoroutine

--- tts/src/androidMain/kotlin/nl/marc_apps/tts/TextToSpeechAndroid.kt
+++ tts/src/androidMain/kotlin/nl/marc_apps/tts/TextToSpeechAndroid.kt
@@ -18,7 +18,7 @@ import nl.marc_apps.tts.utils.getContinuationId
 import java.util.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
+import kotlin.coroutines.suspendCancellableCoroutine
 import android.speech.tts.TextToSpeech as AndroidTTS
 
 /** A TTS instance. Should be [close]d when no longer in use. */
@@ -183,7 +183,7 @@ internal class TextToSpeechAndroid(private var tts: AndroidTTS?) : TextToSpeechI
 
     /** Adds the given [text] to the internal queue, unless [isMuted] is true or [volume] equals 0. */
     override suspend fun say(text: String, clearQueue: Boolean) {
-        suspendCoroutine { cont ->
+        suspendCancellableCoroutine { cont ->
             say(text, clearQueue) {
                 if (it.isSuccess) {
                     cont.resume(Unit)
@@ -192,6 +192,9 @@ internal class TextToSpeechAndroid(private var tts: AndroidTTS?) : TextToSpeechI
                     cont.resumeWithException(error)
                 }
             }
+            cont.invokeOnCancellation {
+                stop()
+            } 
         }
     }
 
