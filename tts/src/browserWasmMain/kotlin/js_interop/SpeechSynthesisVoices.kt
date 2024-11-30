3046f305cc1d513abf2db2e9d4ba9c94c6eaa43b
Marc <16156117+Marc-JB@users.noreply.github.com>
2024-11-30 20:24:09 +0100

    Use new toArray extension

--- tts/src/browserWasmMain/kotlin/js_interop/SpeechSynthesisVoices.kt
+++ tts/src/browserWasmMain/kotlin/js_interop/SpeechSynthesisVoices.kt
@@ -4,6 +4,4 @@ import org.w3c.speech.SpeechSynthesis
 import org.w3c.speech.SpeechSynthesisVoice
 
 /** @hide */
-actual fun getVoiceList(speechSynthesis: SpeechSynthesis) : Array<SpeechSynthesisVoice> {
-    return speechSynthesis.getVoices().iterator().asSequence().filterNotNull().toList().toTypedArray()
-}
+actual fun getVoiceList(speechSynthesis: SpeechSynthesis) = speechSynthesis.getVoices().toArray()
