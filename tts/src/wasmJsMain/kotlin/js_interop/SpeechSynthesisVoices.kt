030deb54bb27c3720248715c912bd8469f282e0c
Marc <16156117+Marc-JB@users.noreply.github.com>
2024-12-12 15:31:20 +0100

    Copy toArray JsArray extension from Kotlin 2.1

--- tts/src/wasmJsMain/kotlin/js_interop/SpeechSynthesisVoices.kt
+++ tts/src/wasmJsMain/kotlin/js_interop/SpeechSynthesisVoices.kt
@@ -5,3 +5,11 @@ import org.w3c.speech.SpeechSynthesisVoice
 
 /** @hide */
 actual fun getVoiceList(speechSynthesis: SpeechSynthesis) = speechSynthesis.getVoices().toArray()
+
+/**
+ * Returns a new [Array] containing all the elements of this [JsArray].
+ */
+private fun <T : JsAny?> JsArray<T>.toArray(): Array<T> {
+    @Suppress("UNCHECKED_CAST", "TYPE_PARAMETER_AS_REIFIED")
+    return Array(this.length) { this[it] as T }
+}
